e1a3418cb9e9ccb6cc0f8ab4beb1578dc29a5fa7
JannisX11 <jannis4236@gmail.com>
2021-11-14 21:25:06 +0100

    WIP custom collada exporter custom XML encoder

--- js/io/formats/collada.js
+++ js/io/formats/collada.js
@@ -6,6 +6,7 @@ var codec = new Codec('collada', {
 	extension: 'dae',
 	async compile(options = 0) {
 		let scope = this;
+		/*
 		let exporter = new THREE.ColladaExporter();
 		let animations = [];
 		let gl_scene = new THREE.Scene();
@@ -27,12 +28,128 @@ var codec = new Codec('collada', {
 				textureDirectory: 'textures'
 			});
 		})
-		scope.dispatchEvent('compile', {model: result.data, options});
-		console.log(result)
-
 		scene.add(Project.model_3d);
+		*/
+		let geometries = [];
+		let root = [];
 
-		return result.data;
+		let model = {
+			type: 'COLLADA',
+			content: [
+				{
+					type: 'asset',
+					content: [
+						{
+							name: 'contributor',
+							content: [
+								{type: 'authoring_tool', content: 'Blockbench'},
+								{type: 'author', content: settings.username.value || 'Blockbench user'}
+							]
+						},
+						{name: 'created', content: new Date().toISOString()},
+						{name: 'modified', content: new Date().toISOString()},
+						{name: 'up_axis', content: 'Y_UP'}
+					]
+				},
+				{
+					type: 'library_geometries',
+					content: geometries
+				},
+				{
+					type: 'library_visual_scenes',
+					content: [{
+						type: visual_scene,
+						attributes: {
+							id: 'Scene',
+							name: 'Scene',
+						},
+						content: root
+					}]
+				},
+				{
+					type: 'scene',
+					content: [{
+						type: 'instance_visual_scene',
+						attributes: {
+							url: '#Scene'
+						}
+					}]
+				}
+			]
+		}
+
+		Cube.all.forEach(cube => {
+			let node = {
+				attributes: {
+					id: cube.uuid,
+					name: cube.name,
+					type: 'NODE'
+				},
+				content: [
+					{type: 'scale', attributes: {sid: 'scale'}, content: '1 1 1'},
+					{type: 'rotate', attributes: {sid: 'rotationX'}, content: '1 0 0 0'},
+					{type: 'rotate', attributes: {sid: 'rotationY'}, content: '0 1 0 0'},
+					{type: 'rotate', attributes: {sid: 'rotationZ'}, content: '0 0 1 -7'},
+					{type: 'translate', attributes: {sid: 'location'}, content: cube.origin.join(' ')},
+					{type: 'instance_geometry', attributes: {url: `#${cube.uuid}-mesh`, name: cube.name}},
+				]
+			}
+			root.push(node);
+
+			let geometry = {
+				attributes: {
+					id: `${cube.uuid}-mesh`,
+					name: Cube.name
+				},
+				content: [{
+					type: 'mesh',
+					content: [
+						{
+							type: 'source',
+							attributes: {id: `${cube.uuid}-mesh-positions`},
+							content: [
+								{
+									type: 'float_array',
+									attributes: {id: `${cube.uuid}-mesh-positions-array`, count: 24},
+									content: cube.mesh.geometry.attributes.position.array.join(' ')
+								}
+							]
+						},
+						{
+							type: 'source',
+							attributes: {id: `${cube.uuid}-mesh-normals`},
+							content: [
+								{
+									type: 'float_array',
+									attributes: {id: `${cube.uuid}-mesh-normals-array`, count: 24},
+									content: cube.mesh.geometry.attributes.normal.array.join(' ')
+								}
+							]
+						},
+						{
+							type: 'vertices',
+							attributes: {id: `${cube.uuid}-mesh-vertices`},
+							content: [
+								{
+									type: 'input',
+									attributes: {semantic: 'POSITION', id: `${cube.uuid}--mesh-positions`}
+								}
+							]
+						}
+					]
+				}]
+			}
+			geometries.push(geometry);
+		})
+
+		scope.dispatchEvent('compile', {model, options});
+		
+
+		if (options.raw) {
+			return model
+		} else {
+			return compileXML(model)
+		}
 	},
 	export() {
 		var scope = codec;
@@ -62,3 +179,42 @@ BARS.defineActions(function() {
 })
 
 })()
+
+function compileXML(object) {
+	let depth = 0;
+	let output = '<?xml version="1.0" encoding="utf-8"?>\n';
+
+	function spaces() {
+		let s = '';
+		for (let i = 0; i < depth; i++) {
+			s += '  ';
+		}
+		return s;
+	}
+	function handleObject(object) {
+		let type = object.type || object.name;
+		let head = `<${type}`;
+		if (object.attributes) {
+			for (let key in object.attributes) {
+				head += ` ${key}="${object.attributes[key]}"`
+			}
+		}
+		output += spaces() + head;
+		if (typeof object.content == 'string') {
+			output += '>' + object.content + `</${type}>\n`;
+		} else if (object.content instanceof Array) {
+			depth++;
+			output += `>\n`;
+			object.content.forEach(node => {
+				handleObject(node);
+			})
+			depth--;
+			output += spaces() + `</${type}>\n`;
+		} else {
+			output += '/>\n';
+		}
+	}
+	handleObject(object);
+
+	return output;
+}
