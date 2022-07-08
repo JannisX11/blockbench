f68ca8e394899d3ff4bf2e32efeafde88f795469
JannisX11 <jannis4236@gmail.com>
2022-07-08 23:33:59 +0200

    Add preview scene 3d model support

--- js/preview/preview_scenes.js
+++ js/preview/preview_scenes.js
@@ -23,6 +23,7 @@ class PreviewScene {
 		PreviewScene.select_options[id] = this.name;
 	}
 	select() {
+		if (PreviewScene.active) PreviewScene.active.unselect();
 		this.preview_models.forEach(model => {
 			model.enable();
 		})
@@ -55,15 +56,19 @@ class PreviewModel {
 		this.model_3d = new THREE.Object3D();
 		this.onUpdate = data.onUpdate;
 
-		this.cubes = data.cubes;
-		this.texture 
+		this.cubes = data.cubes || [];
+		this.texture = data.texture;
+		this.color = data.color;
+		this.texture_size = data.texture_size || [16, 16];
+
+		this.buildModel();
 	}
 	enable() {
 		Canvas.scene.add(this.model_3d);
 		this.update();
 	}
 	disable() {
-		Canvas.scene.add(this.model_3d);
+		Canvas.scene.remove(this.model_3d);
 	}
 	update() {
 		if (typeof this.onUpdate == 'function') {
@@ -72,53 +77,44 @@ class PreviewModel {
 		this.model_3d.visible = !!Condition(this.condition);
 	}
 	buildModel() {
-		var scope = this;
-		if (texture === 'black') {
-			var mat = new THREE.MeshBasicMaterial({color: 0x101013});
-		} else {
-			var img = new Image();
-			img.src = texture;
-			var tex = new THREE.Texture(img);
-			img.tex = tex;
-			img.tex.magFilter = THREE.NearestFilter;
-			img.tex.minFilter = THREE.NearestFilter;
-			img.onload = function() {
-				this.tex.needsUpdate = true;
-			}
+		let tex;
+		if (this.texture) {
+			let img = new Image();
+			img.src = this.texture;
+			tex = new THREE.Texture(img);
+			tex.magFilter = THREE.NearestFilter;
+			tex.minFilter = THREE.NearestFilter;
 			img.crossOrigin = '';
-			var mat = new THREE.MeshLambertMaterial({
-				color: 0xffffff,
-				map: tex,
-				side: 2,
-				alphaTest: 0.05
-			});
+			img.onload = function() {
+				tex.needsUpdate = true;
+			}
 		}
+		this.material = new THREE.MeshLambertMaterial({
+			color: this.color,
+			map: tex,
+			side: 2,
+			alphaTest: 0.05
+		});
 
-		scope.material = mat
+		this.cubes.forEach(cube => {
+			let mesh = new THREE.Mesh(new THREE.BoxGeometry(cube.size[0], cube.size[1], cube.size[2]), this.material)
+			if (cube.origin) {
+				mesh.position.set(cube.origin[0], cube.origin[1], cube.origin[2])
+				mesh.geometry.translate(-cube.origin[0], -cube.origin[1], -cube.origin[2])
+			}
+			mesh.geometry.translate(cube.position[0] + cube.size[0]/2, cube.position[1] + cube.size[1]/2, cube.position[2] + cube.size[2]/2)
+			if (cube.rotation) {
+				mesh.rotation.setFromDegreeArray(cube.rotation)
+			}
 
-		things.forEach(function(s) {
-			var mesh = new THREE.Mesh(new THREE.BoxGeometry(s.size[0], s.size[1], s.size[2]), mat)
-			if (s.origin) {
-				mesh.position.set(s.origin[0], s.origin[1], s.origin[2])
-				mesh.geometry.translate(-s.origin[0], -s.origin[1], -s.origin[2])
-			}
-			mesh.geometry.translate(s.pos[0], s.pos[1], s.pos[2])
-			if (s.rotation) {
-				mesh.rotation.setFromDegreeArray(s.rotation)
-			}
-			if (s.model) {
-				mesh.r_model = s.model
-			}
-			mesh.name = s.name || '';
-
-			function getUVArray(face) {
-				var arr = [
-					[face.uv[0]/16, 1-(face.uv[1]/16)],
-					[face.uv[2]/16, 1-(face.uv[1]/16)],
-					[face.uv[0]/16, 1-(face.uv[3]/16)],
-					[face.uv[2]/16, 1-(face.uv[3]/16)]
+			let getUVArray = (face) => {
+				let arr = [
+					[face.uv[0]/this.texture_size[0], 1-(face.uv[1]/this.texture_size[1])],
+					[face.uv[2]/this.texture_size[0], 1-(face.uv[1]/this.texture_size[1])],
+					[face.uv[0]/this.texture_size[0], 1-(face.uv[3]/this.texture_size[1])],
+					[face.uv[2]/this.texture_size[0], 1-(face.uv[3]/this.texture_size[1])]
 				]
-				var rot = (face.rotation+0)
+				let rot = (face.rotation+0)
 				while (rot > 0) {
 					let a = arr[0];
 					arr[0] = arr[2];
@@ -130,29 +126,26 @@ class PreviewModel {
 				return arr;
 			}
 
-			for (var face in s) {
-				if (s.hasOwnProperty(face) && s[face].uv !== undefined) {
-					var fIndex = 0;
-					switch(face) {
-						case 'north':   fIndex = 10;   break;
-						case 'east':	fIndex = 0;	break;
-						case 'south':   fIndex = 8;	break;
-						case 'west':	fIndex = 2;	break;
-						case 'up':	  fIndex = 4;	break;
-						case 'down':	fIndex = 6;	break;
-					}
-					let uv_array = getUVArray(s[face]);
-					mesh.geometry.attributes.uv.array.set(uv_array[0], fIndex*4 + 0);  //0,1
-					mesh.geometry.attributes.uv.array.set(uv_array[1], fIndex*4 + 2);  //1,1
-					mesh.geometry.attributes.uv.array.set(uv_array[2], fIndex*4 + 4);  //0,0
-					mesh.geometry.attributes.uv.array.set(uv_array[3], fIndex*4 + 6);  //1,0
-					mesh.geometry.attributes.uv.needsUpdate = true;
+			for (let face in cube.faces) {
+				let uv_array = getUVArray(cube.faces[face]);
+				let fIndex = 0;
+				switch(face) {
+					case 'north':   fIndex = 10;	break;
+					case 'east':	fIndex = 0;		break;
+					case 'south':   fIndex = 8;		break;
+					case 'west':	fIndex = 2;		break;
+					case 'up':		fIndex = 4;		break;
+					case 'down':	fIndex = 6;		break;
 				}
+				mesh.geometry.attributes.uv.array.set(uv_array[0], fIndex*4 + 0);  //0,1
+				mesh.geometry.attributes.uv.array.set(uv_array[1], fIndex*4 + 2);  //1,1
+				mesh.geometry.attributes.uv.array.set(uv_array[2], fIndex*4 + 4);  //0,0
+				mesh.geometry.attributes.uv.array.set(uv_array[3], fIndex*4 + 6);  //1,0
+				mesh.geometry.attributes.uv.needsUpdate = true;
 			}
 
-			scope.model.add(mesh);
+			this.model_3d.add(mesh);
 		})
-		scope.model.name = name;
 		return this;
 	}
 }
@@ -166,13 +159,91 @@ PreviewModel.getActiveModels = function() {
 
 
 new PreviewModel('minecraft_overworld', {
-
+	texture: './assets/preview_scenes/mc_overworld.png',
+	texture_size: [32, 32],
+	cubes: [
+		{
+			position: [-8, -16, -8],
+			size: [16, 16, 16],
+			faces: {
+				north: {uv: [0, 16, 16, 32]},
+				south: {uv: [0, 16, 16, 32]},
+				east: {uv: [0, 16, 16, 32]},
+				west: {uv: [0, 16, 16, 32]},
+				up: {uv: [0, 0, 16, 16]},
+				down: {uv: [16, 16, 32, 32]},
+			}
+		}
+	]
 })
 new PreviewModel('minecraft_nether', {
-
+	texture: './assets/preview_scenes/mc_nether.png',
+	texture_size: [32, 32],
+	color: '#ad9c7d',
+	cubes: [
+		{
+			position: [-8, -16, -8],
+			size: [16, 16, 16],
+			faces: {
+				north: {uv: [0, 16, 16, 32]},
+				south: {uv: [0, 16, 16, 32]},
+				east: {uv: [0, 16, 16, 32]},
+				west: {uv: [0, 16, 16, 32]},
+				up: {uv: [0, 0, 16, 16]},
+				down: {uv: [16, 16, 32, 32]},
+			}
+		}
+	]
 })
+let solid_uv = {
+	north: {uv: [0, 0, 16, 16]},
+	south: {uv: [0, 0, 16, 16]},
+	east: {uv: [0, 0, 16, 16]},
+	west: {uv: [0, 0, 16, 16]},
+	up: {uv: [0, 0, 16, 16]},
+	down: {uv: [0, 0, 16, 16]},
+}
 new PreviewModel('minecraft_end', {
-
+	texture: './assets/preview_scenes/mc_end.png',
+	texture_size: [16, 16],
+	color: '#6f8377',
+	cubes: [
+		{
+			position: [-8, -16, -8],
+			size: [16, 16, 16],
+			faces: solid_uv
+		},
+		{
+			position: [8, -16, 8],
+			size: [16, 16, 16],
+			faces: solid_uv
+		},
+		{
+			position: [8, -16, -8],
+			size: [16, 16, 16],
+			faces: solid_uv
+		},
+		{
+			position: [-8, -16, 8],
+			size: [16, 16, 16],
+			faces: solid_uv
+		},
+		{
+			position: [-24, -16, -24],
+			size: [16, 16, 16],
+			faces: solid_uv
+		},
+		{
+			position: [-24, -16, -8],
+			size: [16, 16, 16],
+			faces: solid_uv
+		},
+		{
+			position: [-8, -16, -24],
+			size: [16, 16, 16],
+			faces: solid_uv
+		}
+	]
 })
 new PreviewScene('minecraft_overworld', {
 	preview_models: ['minecraft_overworld']
@@ -184,27 +255,15 @@ new PreviewScene('minecraft_nether', {
 });
 new PreviewScene('minecraft_end', {
 	light_color: {r: 0.45, g: 0.52, b: 0.48},
-	preview_models: ['minecraft_overworld']
+	preview_models: ['minecraft_end']
 });
 
 
 BARS.defineActions(function() {
 	new BarSelect('preview_scene', {
 		category: 'view',
-		//condition: () => Format,
 		value: 'none',
 		options: PreviewScene.select_options,
-		/*
-		options() {
-			let opts = {
-				none: tl('generic.none')
-			}
-			for (let id in PreviewScene.scenes) {
-				let scene = PreviewScene.scenes[id];
-				opts[id] = scene.name;
-			}
-			return opts;
-		},*/
 		onChange() {
 			let scene = PreviewScene.scenes[this.value];
 			if (scene) {
