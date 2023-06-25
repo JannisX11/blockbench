771fe9a7c5aa50475c809bc26ae56f879cbc0a42
JannisX11 <jannis4236@gmail.com>
2023-06-25 12:13:25 +0200

    Multiple loop cuts

--- js/modeling/mesh_editing.js
+++ js/modeling/mesh_editing.js
@@ -1205,9 +1205,9 @@ BARS.defineActions(function() {
 			}
 			let length = getLength();
 
-			function runEdit(amended, offset, direction = 0) {
+			function runEdit(amended, offset, direction = 0, cuts = 1) {
 				Undo.initEdit({elements: Mesh.selected, selection: true}, amended);
-				if (offset == undefined) offset = length / 2;
+				if (offset == undefined) offset = length / (cuts+1);
 				Mesh.selected.forEach(mesh => {
 					let selected_vertices = mesh.getSelectedVertices();
 					let start_face;
@@ -1225,19 +1225,18 @@ BARS.defineActions(function() {
 					let processed_faces = [start_face];
 					let center_vertices_map = {};
 
-					function getCenterVertex(vertices) {
+					function getCenterVertex(vertices, ratio) {
 						let edge_key = vertices.slice().sort().join('.');
 						let existing_key = center_vertices_map[edge_key];
 						if (existing_key) return existing_key;
 
-						let ratio = offset/length;
 						let vector = mesh.vertices[vertices[0]].map((v, i) => Math.lerp(v, mesh.vertices[vertices[1]][i], ratio))
 						let [vkey] = mesh.addVertices(vector);
 						center_vertices_map[edge_key] = vkey;
 						return vkey;
 					}
 
-					function splitFace(face, side_vertices, double_side) {
+					function splitFace(face, side_vertices, double_side, cut_no) {
 						processed_faces.push(face);
 						let sorted_vertices = face.getSortedVertices();
 
@@ -1250,18 +1249,22 @@ BARS.defineActions(function() {
 							let opposite_index_diff = sorted_vertices.indexOf(opposite_vertices[0]) - sorted_vertices.indexOf(opposite_vertices[1]);
 							if (opposite_index_diff == 1 || opposite_index_diff < -2) opposite_vertices.reverse();
 
+							let ratio = offset/length;
+							if (cuts > 1) {
+								ratio = 1 - (1 / (cuts + 1 - cut_no) * ratio * 2);
+							}
 							let center_vertices = [
-								getCenterVertex(side_vertices),
-								getCenterVertex(opposite_vertices)
+								getCenterVertex(side_vertices, ratio),
+								getCenterVertex(opposite_vertices, ratio)
 							]
 
 							let c1_uv_coords = [
-								Math.lerp(face.uv[side_vertices[0]][0], face.uv[side_vertices[1]][0], offset/length),
-								Math.lerp(face.uv[side_vertices[0]][1], face.uv[side_vertices[1]][1], offset/length),
+								Math.lerp(face.uv[side_vertices[0]][0], face.uv[side_vertices[1]][0], ratio),
+								Math.lerp(face.uv[side_vertices[0]][1], face.uv[side_vertices[1]][1], ratio),
 							];
 							let c2_uv_coords = [
-								Math.lerp(face.uv[opposite_vertices[0]][0], face.uv[opposite_vertices[1]][0], offset/length),
-								Math.lerp(face.uv[opposite_vertices[0]][1], face.uv[opposite_vertices[1]][1], offset/length),
+								Math.lerp(face.uv[opposite_vertices[0]][0], face.uv[opposite_vertices[1]][0], ratio),
+								Math.lerp(face.uv[opposite_vertices[0]][1], face.uv[opposite_vertices[1]][1], ratio),
 							];
 
 							let new_face = new MeshFace(mesh, face).extend({
@@ -1284,13 +1287,19 @@ BARS.defineActions(function() {
 							})
 							mesh.addFaces(new_face);
 
+							// Multiple loop cuts
+							if (cut_no+1 < cuts) {
+								splitFace(face, [center_vertices[0], side_vertices[0]], double_side, cut_no+1);
+							}
+
+							if (cut_no != 0) return;
 							// Find next (and previous) face
 							for (let fkey in mesh.faces) {
 								let ref_face = mesh.faces[fkey];
 								if (ref_face.vertices.length < 3 || processed_faces.includes(ref_face)) continue;
 								let vertices = ref_face.vertices.filter(vkey => opposite_vertices.includes(vkey))
 								if (vertices.length >= 2) {
-									splitFace(ref_face, opposite_vertices, ref_face.vertices.length == 4);
+									splitFace(ref_face, opposite_vertices, ref_face.vertices.length == 4, 0);
 									break;
 								}
 							}
@@ -1306,7 +1315,7 @@ BARS.defineActions(function() {
 										
 										if(ref_opposite_vertices.length == 2)
 										{
-											splitFace(ref_face, ref_opposite_vertices, ref_face.vertices.length == 4);
+											splitFace(ref_face, ref_opposite_vertices, ref_face.vertices.length == 4, 0);
 											break;
 										}
 									}
@@ -1323,18 +1332,22 @@ BARS.defineActions(function() {
 								let opposite_index_diff = sorted_vertices.indexOf(opposite_vertices[0]) - sorted_vertices.indexOf(opposite_vertices[1]);
 								if (opposite_index_diff == 1 || opposite_index_diff < -2) opposite_vertices.reverse();
 
+								let ratio = offset/length;
+								if (cuts > 1) {
+									ratio = 1 - (1 / (cuts + 1 - cut_no) * ratio * 2);
+								}
 								let center_vertices = [
-									getCenterVertex(side_vertices),
-									getCenterVertex(opposite_vertices)
+									getCenterVertex(side_vertices, ratio),
+									getCenterVertex(opposite_vertices, ratio)
 								]
 
 								let c1_uv_coords = [
-									Math.lerp(face.uv[side_vertices[0]][0], face.uv[side_vertices[1]][0], offset/length),
-									Math.lerp(face.uv[side_vertices[0]][1], face.uv[side_vertices[1]][1], offset/length),
+									Math.lerp(face.uv[side_vertices[0]][0], face.uv[side_vertices[1]][0], ratio),
+									Math.lerp(face.uv[side_vertices[0]][1], face.uv[side_vertices[1]][1], ratio),
 								];
 								let c2_uv_coords = [
-									Math.lerp(face.uv[opposite_vertices[0]][0], face.uv[opposite_vertices[1]][0], offset/length),
-									Math.lerp(face.uv[opposite_vertices[0]][1], face.uv[opposite_vertices[1]][1], offset/length),
+									Math.lerp(face.uv[opposite_vertices[0]][0], face.uv[opposite_vertices[1]][0], ratio),
+									Math.lerp(face.uv[opposite_vertices[0]][1], face.uv[opposite_vertices[1]][1], ratio),
 								];
 
 								let other_quad_vertex = side_vertices.find(vkey => !opposite_vertices.includes(vkey));
@@ -1364,13 +1377,19 @@ BARS.defineActions(function() {
 								}
 								mesh.addFaces(new_face);
 
+								// Multiple loop cuts
+								if (cut_no+1 < cuts) {
+									splitFace(face, [center_vertices[0], other_quad_vertex], double_side, cut_no+1);
+								}
+
+								if (cut_no != 0) return;
 								// Find next (and previous) face
 								for (let fkey in mesh.faces) {
 									let ref_face = mesh.faces[fkey];
 									if (ref_face.vertices.length < 3 || processed_faces.includes(ref_face)) continue;
 									let vertices = ref_face.vertices.filter(vkey => opposite_vertices.includes(vkey))
 									if (vertices.length >= 2) {
-										splitFace(ref_face, opposite_vertices, ref_face.vertices.length == 4);
+										splitFace(ref_face, opposite_vertices, ref_face.vertices.length == 4, 0);
 										break;
 									}
 								}
@@ -1385,7 +1404,7 @@ BARS.defineActions(function() {
 											let ref_opposite_vertices = ref_sorted_vertices.filter(vkey => !side_vertices.includes(vkey));
 											
 											if (ref_opposite_vertices.length == 2) {
-												splitFace(ref_face, ref_opposite_vertices, ref_face.vertices.length == 4);
+												splitFace(ref_face, ref_opposite_vertices, ref_face.vertices.length == 4, 0);
 												break;
 											}
 										}
@@ -1394,11 +1413,15 @@ BARS.defineActions(function() {
 							} else {
 								let opposite_vertex = sorted_vertices.find(vkey => !side_vertices.includes(vkey));
 
-								let center_vertex = getCenterVertex(side_vertices);
+								let ratio = offset/length;
+								if (cuts > 1) {
+									ratio = 1 - (1 / (cuts + 1 - cut_no) * ratio * 2);
+								}
+								let center_vertex = getCenterVertex(side_vertices, ratio);
 
 								let c1_uv_coords = [
-									Math.lerp(face.uv[side_vertices[0]][0], face.uv[side_vertices[1]][0], offset/length),
-									Math.lerp(face.uv[side_vertices[0]][1], face.uv[side_vertices[1]][1], offset/length),
+									Math.lerp(face.uv[side_vertices[0]][0], face.uv[side_vertices[1]][0], ratio),
+									Math.lerp(face.uv[side_vertices[0]][1], face.uv[side_vertices[1]][1], ratio),
 								];
 
 								let new_face = new MeshFace(mesh, face).extend({
@@ -1425,11 +1448,15 @@ BARS.defineActions(function() {
 							}
 						} else if (face.vertices.length == 2) {
 
-							let center_vertex = getCenterVertex(side_vertices);
+							let ratio = offset/length;
+							if (cuts > 1) {
+								ratio = 1 - (1 / (cuts + 1 - cut_no) * ratio * 2);
+							}
+							let center_vertex = getCenterVertex(side_vertices, ratio);
 
 							let c1_uv_coords = [
-								Math.lerp(face.uv[side_vertices[0]][0], face.uv[side_vertices[1]][0], offset/length),
-								Math.lerp(face.uv[side_vertices[0]][1], face.uv[side_vertices[1]][1], offset/length),
+								Math.lerp(face.uv[side_vertices[0]][0], face.uv[side_vertices[1]][0], ratio),
+								Math.lerp(face.uv[side_vertices[0]][1], face.uv[side_vertices[1]][1], ratio),
 							];
 
 							let new_face = new MeshFace(mesh, face).extend({
@@ -1454,7 +1481,7 @@ BARS.defineActions(function() {
 					let start_edge = [start_vertices[direction % start_vertices.length], start_vertices[(direction+1) % start_vertices.length]];
 					if (start_edge.length == 1) start_edge.splice(0, 0, start_vertices[0]);
 
-					splitFace(start_face, start_edge, start_face.vertices.length == 4 || direction > 2);
+					splitFace(start_face, start_edge, start_face.vertices.length == 4 || direction > 2, 0);
 
 					selected_vertices.empty();
 					for (let key in center_vertices_map) {
@@ -1469,7 +1496,7 @@ BARS.defineActions(function() {
 
 			Undo.amendEdit({
 				direction: {type: 'number', value: 0, label: 'edit.loop_cut.direction', condition: !!selected_face, min: 0},
-				//cuts: {type: 'number', value: 1, label: 'edit.loop_cut.cuts', min: 0, max: 16},
+				cuts: {type: 'number', value: 1, label: 'edit.loop_cut.cuts', min: 0, max: 16},
 				offset: {type: 'number', value: length/2, label: 'edit.loop_cut.offset', min: 0, max: length, interval_type: 'position'},
 			}, (form, form_options) => {
 				let direction = form.direction || 0;
@@ -1483,7 +1510,7 @@ BARS.defineActions(function() {
 					saved_direction = direction;
 				}
 				
-				runEdit(true, form_options.offset.slider.value, form_options.direction ? direction : 0);
+				runEdit(true, form_options.offset.slider.value, form_options.direction ? direction : 0, form.cuts);
 			})
 		}
 	})
