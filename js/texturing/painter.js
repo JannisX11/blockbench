9142c1a9853052ae9e9f98a98e7e54ca0e9c1f0b
JannisX11 <jannis4236@gmail.com>
2023-11-04 19:06:20 +0100

    Pixel perfect brush option

--- js/texturing/painter.js
+++ js/texturing/painter.js
@@ -902,7 +902,7 @@ const Painter = {
 			interval = Math.sqrt(Math.pow(diff_x/diff_y, 2) + 1)
 		}
 
-		if (Toolbox.selected.brush?.pixel_perfect && BarItems.slider_brush_size.get() == 1) {
+		if (Toolbox.selected.brush?.pixel_perfect && BarItems.pixel_perfect_drawing.value && BarItems.slider_brush_size.get() == 1) {
 			let direction = 0;
 			if (length == 1 && diff_x && !diff_y) {direction = 1;}
 			if (length == 1 && !diff_x && diff_y) {direction = 2;}
@@ -1572,6 +1572,7 @@ const Painter = {
 							softness: preset.softness == null ? BarItems.slider_brush_softness.get() : preset.softness,
 							use_opacity: preset.opacity !== null,
 							opacity: preset.opacity == null ? BarItems.slider_brush_opacity.get() : preset.opacity,
+							pixel_perfect: preset.pixel_perfect == null ? BarItems.pixel_perfect_drawing.value : preset.pixel_perfect,
 							use_color: preset.color !== null,
 							color: preset.color == null ? ColorPanel.get() : preset.color,
 							shape: preset.shape ? preset.shape : 'unset',
@@ -1638,6 +1639,7 @@ const Painter = {
 				opacity: {label: ' ', nocolon: true, description: 'action.slider_brush_opacity.desc', type: 'number', condition: form => form.use_opacity, value: 255, min: 0, max: 255},
 				use_softness: {label: 'action.slider_brush_softness', description: 'action.slider_brush_softness.desc', type: 'checkbox'},
 				softness: {label: ' ', nocolon: true, description: 'action.slider_brush_softness.desc', type: 'number', condition: form => form.use_softness, value: 0, min: 0, max: 100},
+				pixel_perfect: {label: 'action.pixel_perfect_drawing', type: 'checkbox'},
 				use_color: {label: 'data.color', type: 'checkbox'},
 				color: {label: ' ', nocolon: true, description: 'action.brush_shape.desc', type: 'color', condition: form => form.use_color},
 				actions: {type: 'buttons', buttons: ['generic.delete'], click() {
@@ -1697,6 +1699,7 @@ const Painter = {
 		if (typeof preset.size == 'number') 	BarItems.slider_brush_size.setValue(preset.size);
 		if (typeof preset.softness == 'number') BarItems.slider_brush_softness.setValue(preset.softness);
 		if (typeof preset.opacity == 'number') 	BarItems.slider_brush_opacity.setValue(preset.opacity);
+		if (preset.pixel_perfect != undefined) 	BarItems.pixel_perfect_drawing.set(preset.pixel_perfect);
 		if (preset.color) 		ColorPanel.set(preset.color);
 		if (preset.shape) {
 			BarItems.brush_shape.set(preset.shape);
@@ -1717,6 +1720,16 @@ const Painter = {
 			default: true,
 			size: 1,
 			softness: 0,
+			pixel_perfect: false,
+			shape: 'square',
+			blend_mode: 'default'
+		},
+		{
+			name: 'menu.brush_presets.pixel_perfect',
+			default: true,
+			size: 1,
+			softness: 0,
+			pixel_perfect: false,
 			shape: 'square',
 			blend_mode: 'default'
 		},
@@ -1725,6 +1738,7 @@ const Painter = {
 			default: true,
 			size: 5,
 			softness: 70,
+			pixel_perfect: false,
 			shape: 'circle',
 			blend_mode: 'default'
 		}
@@ -1799,9 +1813,13 @@ BARS.defineActions(function() {
 					BarItems.brush_tool.select();
 					Painter.loadBrushPreset(Painter.default_brush_presets[0])
 				}},
-				{name: 'menu.brush_presets.smooth_brush', icon: 'fa-paint-brush', click() {
+				{name: 'menu.brush_presets.pixel_perfect_brush', icon: 'stylus_note', click() {
 					BarItems.brush_tool.select();
 					Painter.loadBrushPreset(Painter.default_brush_presets[1])
+				}},
+				{name: 'menu.brush_presets.smooth_brush', icon: 'fa-paint-brush', click() {
+					BarItems.brush_tool.select();
+					Painter.loadBrushPreset(Painter.default_brush_presets[2])
 				}},
 			];
 			StateMemory.brush_presets.forEach((preset) => {
@@ -2417,5 +2435,10 @@ BARS.defineActions(function() {
 				}
 			}
 		}
+	})
+	new Toggle('pixel_perfect_drawing', {
+		icon: 'stylus_laser_pointer',
+		category: 'view',
+		condition: () => Toolbox && Toolbox.selected.brush?.pixel_perfect == true,
 	})
 })
