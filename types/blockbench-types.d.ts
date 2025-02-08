2092e95c1c9ab894a8b4eaaa4fe47b50e989b20a
SnaveSutit <snavesutit@gmail.com>
2025-02-08 16:11:58 -0500

    Improve `process.env` typing
    This file might be moved to `blockbench-types` later?
    Whether or not that happens will be up to @JannisX11

--- types/blockbench-types.d.ts
+++ types/blockbench-types.d.ts
@@ -2,24 +2,22 @@
 
 // Define our custom env variables
 declare namespace NodeJS {
-	interface Process {
-		env: {
-			/**
-			 * Whether to enable auto-updates
-			 */
-			BLOCKBENCH_AUTO_UPDATE?: 'ENABLED' | 'DISABLED'
-			/**
-			 * The path to the user data directory
-			 */
-			BLOCKBENCH_USER_DATA?: string
-			/**
-			 * A comma-separated list of plugin file paths
-			 */
-			BLOCKBENCH_PLUGIN_FILES?: string
-			/**
-			 * A comma-separated list of plugin URLs
-			 */
-			BLOCKBENCH_PLUGIN_URLS?: string
-		}
+	export interface ProcessEnv {
+		/**
+		 * Whether to enable auto-updates
+		 */
+		BLOCKBENCH_AUTO_UPDATE?: 'ENABLED' | 'DISABLED'
+		/**
+		 * The path to the user data directory
+		 */
+		BLOCKBENCH_USER_DATA?: string
+		/**
+		 * A comma-separated list of plugin file paths
+		 */
+		BLOCKBENCH_PLUGIN_FILES?: string
+		/**
+		 * A comma-separated list of plugin URLs
+		 */
+		BLOCKBENCH_PLUGIN_URLS?: string
 	}
 }
