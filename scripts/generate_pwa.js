efd19dc15b5e473b1c6484df1888df8a1cf5c87f
JannisX11 <jannis4236@gmail.com>
2025-10-21 23:50:41 +0200

    Fix web app not caching correctly for offline use

--- scripts/generate_pwa.js
+++ scripts/generate_pwa.js
@@ -15,7 +15,7 @@ workbox.generateSW({
 		'./font/*',
 	],
 	swDest: './service_worker.js',
-	maximumFileSizeToCacheInBytes: 4_096_000,
+	maximumFileSizeToCacheInBytes: 32_096_000,
 	sourcemap: false
 }).then(({count, size}) => {
 	console.log(`Generated service-worker, which will precache ${count} files, totaling ${(size/1e6).toFixed(2)} MB.`);
