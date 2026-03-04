b7b7f03f94d5df12f87b2a79d7db5e5b369faae1
SnaveSutit <SnaveSutit@gmail.com>
2026-03-04 17:26:37 -0500

    ✨ Improved `generate-types` script.
    - Renamed `convert_types.js` -> `generate_types.js`.
    - Converted `generate_types.js` to TypeScript.
    - Added `--watch` argument to `generate-types`.
    - Added support for Regex to the `excluded` list in `type_config.json`.

---
+++ scripts/generate_types.ts
@@ -0,0 +1,195 @@
+import chokidar from 'chokidar'
+import { spawn } from 'child_process'
+import { readdir, readFile, stat, unlink, writeFile } from 'fs/promises'
+import config from '../types/type_config.json' with { type: 'json' }
+import chalk from 'chalk'
+import { join, relative } from 'path'
+
+const TYPE_EXCLUSION_PATTERNS = config.exclude.map(expression => new RegExp(expression))
+const GENERATED_TYPES_DIR = './types/generated'
+
+/**
+ * Convert all auto-generated type definitions to declare globally instead of exporting
+ * Removes empty files and excluded files
+ */
+
+let working = false
+let processedFileCount = 0
+let deletedFileCount = 0
+
+function isComment(line: string) {
+	return line.startsWith('/*') || line.startsWith(' *') || line.startsWith('//')
+}
+
+async function compileTypeDefinitions() {
+	console.log(chalk.blue('⚙️  Compiling type definitions...\n'))
+	return new Promise<void>((resolve, reject) =>
+		spawn('tsc', ['--project', 'tsconfig.json'], { stdio: 'inherit' })
+			.on('close', code => {
+				if (code !== 0) {
+					console.error(
+						chalk.red('\n🚨 TypeScript compilation compiled with type warnings!')
+					)
+				} else {
+					console.log(chalk.blue('\n✅ Type definitions compiled successfully.\n'))
+				}
+				resolve()
+			})
+			.on('error', error => {
+				console.error(
+					chalk.red('⚠️  Unexpected error during type definition compilation:'),
+					error
+				)
+				reject(error)
+			})
+	)
+}
+
+function transformFileContents(content: string, path: string) {
+	if (content.match(/\ndeclare global {/)) return content
+
+	// TODO: Handle comments
+	let lines = content.split(/\r?\n/)
+	let output_lines = []
+	let i = 0
+	let global_scope = false
+	let comment_stash = []
+	for (let line of lines) {
+		if (global_scope) {
+			if (line.startsWith('}') || line.startsWith('  ')) {
+				output_lines.push('    ' + line)
+			} else if (
+				(line.startsWith('export ') || line.startsWith('interface')) &&
+				!line.startsWith('export {}')
+			) {
+				for (let comment of comment_stash) {
+					output_lines.push('    ' + comment)
+				}
+				comment_stash.length = 0
+				let shorter_line = line.replace(/^export (default )?(declare )?/, '')
+				output_lines.push('    ' + shorter_line)
+			} else if (isComment(line)) {
+				// Comment
+				comment_stash.push(line)
+			} else {
+				output_lines.push('}')
+				global_scope = false
+				output_lines.push(line)
+			}
+		} else if (
+			(line.startsWith('export ') || line.startsWith('interface')) &&
+			!line.startsWith('export {}')
+		) {
+			output_lines.push('declare global {')
+			for (let comment of comment_stash) {
+				output_lines.push('    ' + comment)
+			}
+			comment_stash.length = 0
+			let shorter_line = line.replace(/^export (default )?(declare )?/, '')
+			output_lines.push('    ' + shorter_line)
+			global_scope = true
+		} else if (isComment(line)) {
+			// Comment
+			comment_stash.push(line)
+		} else {
+			if (comment_stash.length) {
+				output_lines.push(...comment_stash)
+				comment_stash.length = 0
+			}
+			if (line) output_lines.push(line)
+		}
+		i++
+	}
+	if (output_lines.includes('export {};') == false) {
+		output_lines.push('export {};')
+	}
+	output_lines = output_lines.map(line => {
+		return line.replace(/, }/g, ' }')
+	})
+	output_lines.push('')
+	let result = output_lines.join('\n')
+	return result
+}
+
+function isEmptyFile(content: string) {
+	return content.replace('export {};', '').trim().length == 0
+}
+
+function normalizePath(path: string) {
+	return path.replace(/[\\\/]/g, '/')
+}
+
+function isExcluded(path: string) {
+	const relativePath = normalizePath(relative(GENERATED_TYPES_DIR, path))
+	return TYPE_EXCLUSION_PATTERNS.some(pattern => !!relativePath.match(pattern))
+}
+
+async function processFile(path: string) {
+	const fileContent = await readFile(path, { encoding: 'utf-8' })
+	const processedContent = transformFileContents(fileContent, path)
+
+	if (!processedContent || isEmptyFile(processedContent) || isExcluded(path)) {
+		await unlink(path)
+		deletedFileCount++
+		return
+	}
+
+	if (processedContent != fileContent) {
+		await writeFile(path, processedContent, { encoding: 'utf-8' })
+		processedFileCount++
+	}
+}
+
+async function processDirectoryContents(path: string) {
+	for (const item of await readdir(path)) {
+		const itemPath = join(path, item)
+		const stats = await stat(itemPath)
+
+		if (stats.isDirectory()) {
+			await processDirectoryContents(itemPath)
+		} else {
+			await processFile(itemPath)
+		}
+	}
+}
+
+async function convertTypes() {
+	processedFileCount = 0
+	deletedFileCount = 0
+	await compileTypeDefinitions()
+	await processDirectoryContents(GENERATED_TYPES_DIR)
+	console.log(
+		chalk.green(
+			`\n✅ Done! ${chalk.cyan(processedFileCount)} files processed, ${chalk.red(deletedFileCount)} files deleted.`
+		)
+	)
+}
+
+const onUpdate = async (path: string) => {
+	if (working) return // Prevent multiple simultaneous executions
+	working = true
+
+	console.log(chalk.blue(`📝 Change detected: ${path}\n`))
+	try {
+		await convertTypes()
+	} finally {
+		working = false
+	}
+}
+
+async function main() {
+	await convertTypes()
+
+	if (process.argv.includes('--watch')) {
+		chokidar
+			.watch('./js', { ignoreInitial: true, awaitWriteFinish: true })
+			.on('ready', () => {
+				console.log(chalk.blue('👀 Watching for changes...'))
+			})
+			.on('change', onUpdate)
+			.on('add', onUpdate)
+			.on('unlink', onUpdate)
+	}
+}
+
+void main()
