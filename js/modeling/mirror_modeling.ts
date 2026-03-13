9ef01e3c7d212bfafd18202d7b979c0116126ca3
JannisX11 <jannis4236@gmail.com>
2026-03-13 20:29:11 +0100

    Fix #3379 Moving Group Pivot While in Mirror Mode Not Undo-able for Mirrored Side

--- js/modeling/mirror_modeling.ts
+++ js/modeling/mirror_modeling.ts
@@ -61,15 +61,17 @@ export const MirrorModeling = {
 			}
 
 			// Update hierarchy up
-			function updateParent(child, child_b) {
+			function updateParent(child: OutlinerNode, child_b: OutlinerNode) {
 				let parent = child.parent;
 				let parent_b = child_b.parent;
-				if (parent == parent_b) return;
+				if (parent == parent_b || parent == Outliner.ROOT || parent_b == Outliner.ROOT) return;
 				if (parent.type != parent_b.type) return;
 				if (parent instanceof OutlinerNode == false || parent.getTypeBehavior('parent') != true) return;
 				if (parent_b instanceof OutlinerNode == false || parent_b.getTypeBehavior('parent') != true) return;
 
+				let before_snapshot = parent_b instanceof Group ? parent_b.getSaveCopy() : undefined;
 				MirrorModeling.updateParentNodeCounterpart(parent_b, parent);
+				if (parent_b instanceof Group) MirrorModeling.insertGroupIntoUndo(parent_b, undo_aspects, before_snapshot);
 
 				updateParent(parent, parent_b);
 			}
@@ -233,7 +235,7 @@ export const MirrorModeling = {
 		// pre
 		if (!Undo.current_save.groups) Undo.current_save.groups = [];
 		if (before_snapshop) {
-			if (Undo.current_save.groups.find((g: any) => g.uuid == before_snapshop.uuid)) {
+			if (!Undo.current_save.groups.find((g: any) => g.uuid == before_snapshop.uuid)) {
 				Undo.current_save.groups.push(before_snapshop);
 			}
 		} else {
