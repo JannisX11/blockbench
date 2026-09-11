f48e898334410d16732cb58f62c8abf2a059d618
Ewan Howell <48070995+ewanhowell5195@users.noreply.github.com>
2026-09-11 13:38:33 +0100

    add close tabs to the left action

--- js/io/project.ts
+++ js/io/project.ts
@@ -701,6 +701,7 @@ ModelProject.prototype.menu = new Menu([
 	'convert_project',
 	'close_project',
 	'close_other_projects',
+	'close_projects_to_left',
 	'close_projects_to_right',
 	'close_all_projects',
 	new MenuSeparator('save'),
@@ -1146,6 +1147,11 @@ onVueSetup(() => {
 })
 
 
+function hasTabsOnBothSides() {
+	let index = ModelProject.all.indexOf(Project);
+	return index > 0 && index < ModelProject.all.length-1;
+}
+
 BARS.defineActions(function() {
 
 	new Action('project_window', {
@@ -1315,10 +1321,33 @@ BARS.defineActions(function() {
 			}
 		}
 	})
+	new Action('close_projects_to_left', {
+		icon() {
+			let node = Blockbench.getIconNode('tab_close_right');
+			node.classList.add('flipped');
+			return node;
+		},
+		category: 'file',
+		condition: hasTabsOnBothSides,
+		async click() {
+			let projects = ModelProject.all.slice(0, ModelProject.all.indexOf(Project));
+			if (projects.some(project => !project.saved)) {
+				try {
+					await showUnsavedWorkDialog(projects);
+				} catch (err) {
+					return;
+				}
+			}
+			for (let project of projects) {
+				if (!ModelProject.all.includes(project)) continue;
+				await project.close(true);
+			}
+		}
+	})
 	new Action('close_projects_to_right', {
 		icon: 'tab_close_right',
 		category: 'file',
-		condition: () => ModelProject.all.length > 1 && ModelProject.all.indexOf(Project) != ModelProject.all.length-1,
+		condition: hasTabsOnBothSides,
 		async click() {
 			let index = ModelProject.all.indexOf(Project);
 			let projects = ModelProject.all.slice(index+1);
