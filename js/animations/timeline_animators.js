078e41f034c369b70a70ca73adee8f55f88f09ba
JannisX11 <jannis4236@gmail.com>
2026-09-19 17:57:22 +0200

    Fix IK glitch when bones and pole are fully in-line

--- js/animations/timeline_animators.js
+++ js/animations/timeline_animators.js
@@ -915,6 +915,9 @@ export class NullObjectAnimator extends BoneAnimator {
 		let pole_pos;
 		if (pole) {
 			pole_pos = pole.mesh.getWorldPosition(new THREE.Vector3());
+			// Fix glitches when perfectly in-line
+			pole_pos.x += 0.0017;
+			pole_pos.z += 0.0017;
 		}
 
 		fabrikIter(bone_pos, ik_target, pole_pos);
